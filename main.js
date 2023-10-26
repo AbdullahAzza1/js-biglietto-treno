44bfd9bc536b6193db147ddca6f4870e5df78291
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-10-26 21:35:00 +0200

    step7

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ const numChilometri = parseInt(prompt("quanti km vuoi percorrere?"));
 const eta = parseInt(prompt("eta del passeggero"));
 const tariffa = 0.21;
 const percorrenza = numChilometri * tariffa;
-document.getElementById("biglietto").innerHTML = 'il biglietto e ' + percorrenza + 'euro';
 console.log(percorrenza);
 
 if ("sotto i 18"){
     document.getElementById("minorenni").innerHTML = percorrenza / 100 * 80 + 'euro';
-} else if ("sopra i 65"){
+} else if ("over65"){
     document.getElementById("over65").innerHTML = percorrenza / 100 * 60 + 'euro';
 }
-    
+console.log(percorrenza.toFixed(2))
+document.getElementById("biglietto").innerHTML = 'il biglietto e di ' + (percorrenza.toFixed(2)) + 'euro';
+document.getElementById("minorenni").innerHTML = 'il biglietto under18 ' + (percorrenza.toFixed(1)) / 100 * 80 + 'euro';
+document.getElementById("over65").innerHTML = 'il biglietto over65 ' + (percorrenza.toFixed(2)) / 100 * 60 + 'euro';
+
+   
 
 
 
