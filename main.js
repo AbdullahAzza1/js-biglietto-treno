0797f27e140a4de1feec114d1d31a40999c603fb
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-10-26 17:32:20 +0200

    step4

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const numChilometri = prompt("quanti km vuoi percorrere?");
 const chilometri = numChilometri();
 const tariffa = tariffa(0.21);
 
-document.getElementById("biglietto").innerHTML = 'chilometri, * tariffe';
+document.getElementById("biglietto").innerHTML = 'chilometri * tariffe';
 
 
 
